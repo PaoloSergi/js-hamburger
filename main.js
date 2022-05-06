f7c890175fa78b9eacfe8660367ef925ea10b389
PaoloGit <paolo.sergi.88@gmail.com>
2022-05-06 16:48:27 +0200

    aggiunto logo e cambiato min-height

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const openMenuIcon = document.querySelector('.header-right .fa-bars');
 const closeMenuIcon = document.querySelector('.hamburger-menu .fa-times');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.style.height = "200px";
+hamburgerMenu.style.minHeight = "200px";
 
 openMenuIcon.addEventListener ('click',
     function () {
-        document.querySelector('header .hamburger-menu').classList.add ("active");
+        hamburgerMenu.classList.add ("active");
     }
 );
 
 closeMenuIcon.addEventListener ('click',
     function () {
-        document.querySelector('header .hamburger-menu.active').className = "hamburger-menu";
+        hamburgerMenu.className = "hamburger-menu";
     }
 );
+
